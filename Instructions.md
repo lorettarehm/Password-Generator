2079a589386b49e627e72feedbe9413c30b0d5bb
LR.btcmp <loretta.rehm@gmail.com>
2022-11-18 20:07:49 +0000

    Challenge logic finished

---
+++ Instructions.md
@@ -0,0 +1,79 @@
+# Module 5 Challenge: Password Generator
+
+## Overview
+
+This week’s challenge requires you to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+
+The password can include special characters. If you’re unfamiliar with these, see this [list of Password Special Characters from the OWASP Foundation](https://www.owasp.org/index.php/Password_special_characters).
+
+## Instructions
+
+The following image shows the web application's appearance and functionality:
+
+![password generator demo](./assets/05-javascript-challenge-demo.png)
+
+
+* Generate a password when the button is clicked
+  * Present a series of prompts for password criteria
+    * Length of password
+      * At least 10 characters but no more than 64.
+    * Character types
+      * Lowercase
+      * Uppercase
+      * Numeric
+      * Special characters ($@%&*, etc)
+  * Code should validate for each input and at least one character type should be selected
+  * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+
+## Grading Requirements
+
+This challenge is graded based on the following criteria: 
+
+### Technical Acceptance Criteria: 40%
+
+* Satisfies all of the above acceptance criteria plus the following:
+
+  * The challenge should not produce any errors in the console when you inspect it using Chrome DevTools.
+
+### Deployment: 32%
+
+* Application deployed at live URL.
+
+* Application loads with no errors.
+
+* Application GitHub URL submitted.
+
+* GitHub repository that contains application code.
+
+### Application Quality: 15%
+
+* Application user experience is intuitive and easy to navigate.
+
+* Application user interface style is clean and polished.
+
+* Application resembles the mock-up functionality provided in the challenge instructions.
+
+### Repository Quality: 13%
+
+* Repository has a unique name.
+
+* Repository follows best practices for file structure and naming conventions.
+
+* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
+
+* Repository contains multiple descriptive commit messages.
+
+* Repository contains quality README file with description, screenshot, and link to deployed application.
+
+
+## Review
+
+You are required to submit the following for review:
+
+* The URL of the deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+---
+
+© 2022 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,106 +1,173 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9'
+];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
 ];
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
+    // Variable for concatenated array that contain all options chosen by the user
+    var arrDomain = [];
+
+    // Variable to check if there are no options chosen
+    var noOptionChosen = true;
+
+    // Check if user wants lower case characters
+    if (confirm('Do you want to include lowercase letters?')) {
+        noOptionChosen = false;
+        arrDomain = arrDomain.concat(lowerCasedCharacters);
+    }
+
+    // Check if user wants upper case characters
+    if (confirm('Do you want to include uppercase letters?')) {
+        noOptionChosen = false;
+        arrDomain = arrDomain.concat(upperCasedCharacters);
+    }
+
+    // Check if user wants numeric characters
+    if (confirm('Do you want to include numeric characters?')) {
+        noOptionChosen = false;
+        arrDomain = arrDomain.concat(numericCharacters);
+    }
+
+    // Check if user wants special characters
+    if (confirm('Do you want to include special characters?')) {
+        noOptionChosen = false;
+        arrDomain = arrDomain.concat(specialCharacters);
+    }
+
+    // Check if user chose at least one of the options
+    if (noOptionChosen) {
+        alert('You have to choose at least one option.');
+    } else {
+      return arrDomain;
+    }
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom(arr) { // Get index with random
 
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // Get item on array that corresponds to the index
+    const item = arr[randomIndex];
+
+    return item;
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(arrDomain) {
+  var userQtCharacter = prompt('How many characters you want? Minimum is 10 and maximum is 64.');
 
+  var finalPassword = '';
+  // Validate if quantity of characters chosen by the user is a number
+  if (isNaN(Number(userQtCharacter))) {
+    alert("Invalide value, please select a number.");
+  } else if (Number(userQtCharacter) < 10 || Number(userQtCharacter) > 64) {
+    alert("Invalide value, please select a number between 10 and 64.");
+  } else {
+      for (var i = 0; i < Number(userQtCharacter); i++) {
+          finalPassword += getRandom(arrDomain);
+      }
+
+      return finalPassword;
+  }
 }
 
 // Get references to the #generate element
@@ -108,10 +175,11 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+    
+    var password = generatePassword(getPasswordOptions());
+    var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
